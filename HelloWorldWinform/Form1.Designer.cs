faf467062c43f5266d9d7d61fdd4b866a2edf780
mic050r <s2216@e-mirim.hs.kr>
2024-03-25 14:29:15 +0900

    Form 제목 변경

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(992, 629);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form1545454";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
