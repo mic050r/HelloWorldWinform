7eb425e1908e2bde15a577922642904510ec439a
mic050r <s2216@e-mirim.hs.kr>
2024-03-25 15:01:11 +0900

    파비콘 이미지 추가

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -66,6 +66,7 @@
             this.Controls.Add(this.label1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "3214 임지현 3-2 급훈";
